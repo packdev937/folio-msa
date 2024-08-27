f3d15357c3d5fa8d0cb840a496b941526a91e7dd
Geonwoo Pack <packdev937@gmail.com>
2024-08-27 14:04:54 +0900

    feat : Custom Annotation을 생성한다

---
+++ user-service/src/main/java/kr/folio/user/infrastructure/annotation/Adapter.java
@@ -0,0 +1,8 @@
+package kr.folio.user.infrastructure.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public @interface Adapter {
+
+}
---
+++ user-service/src/main/java/kr/folio/user/infrastructure/annotation/ApplicationService.java
@@ -0,0 +1,16 @@
+package kr.folio.user.infrastructure.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Service
+@Transactional(readOnly = true)
+public @interface ApplicationService {
+
+}
