a83f7164f412df2e988729ee76ad1a4db8352ca0
Geonwoo Pack <packdev937@gmail.com>
2024-08-30 14:36:01 +0900

    refactor : UserController의 log 및 parameters를 리팩토링 한다

--- user-service/src/main/java/kr/folio/user/presentation/rest/UserController.java
+++ user-service/src/main/java/kr/folio/user/presentation/rest/UserController.java
@@ -1,5 +1,7 @@
 package kr.folio.user.presentation.rest;
 
+import java.util.HashMap;
+import java.util.Map;
 import kr.folio.user.application.ports.input.UserApplicationUseCase;
 import kr.folio.user.presentation.dto.request.CreateUserRequest;
 import kr.folio.user.presentation.dto.request.UpdateBirthdayRequest;
@@ -8,6 +10,7 @@ import kr.folio.user.presentation.dto.request.UpdateNicknameRequest;
 import kr.folio.user.presentation.dto.request.UpdateProfileImageUrlRequest;
 import kr.folio.user.presentation.dto.response.CreateUserResponse;
 import kr.folio.user.presentation.dto.response.DeleteUserResponse;
+import kr.folio.user.presentation.dto.response.RetrieveUserHomeResponse;
 import kr.folio.user.presentation.dto.response.UpdateUserResponse;
 import kr.folio.user.presentation.dto.response.ValidateUserResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,19 +28,26 @@ public class UserController implements UserControllerDocs {
     private final UserApplicationUseCase userApplicationUseCase;
 
     @GetMapping("/health_check")
-    public String healthCheck() {
-        return "User-Service is working.";
+    public Map<String, Object> healthCheck() {
+        Map<String, Object> healthDetails = new HashMap<>();
+        healthDetails.put("status", "User-Service가 실행 중 입니다.");
+        healthDetails.put("serverTime", System.currentTimeMillis());
+        healthDetails.put("freeMemory", Runtime.getRuntime().freeMemory());
+
+        return healthDetails;
     }
 
     @Override
     public ResponseEntity<CreateUserResponse> createUser(CreateUserRequest request) {
         log.info("Creating user with id : {} nickname : {}", request.id(), request.nickname());
+
         return new ResponseEntity<>(userApplicationUseCase.createUser(request), HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<ValidateUserResponse> validateUserId(String id) {
-        log.info("Validating user id : {}", id);
+        log.info("Validating duplicated user. ID : {}", id);
+
         return new ResponseEntity<>(
             userApplicationUseCase.validateUserId(id),
             HttpStatus.OK
@@ -46,7 +56,8 @@ public class UserController implements UserControllerDocs {
 
     @Override
     public ResponseEntity<ValidateUserResponse> validateUserNickname(String nickname) {
-        log.info("Validating user nickname : {}", nickname);
+        log.info("Validating duplicated user nickname. Nickname : {}", nickname);
+
         return new ResponseEntity<>(
             userApplicationUseCase.validateUserNickname(nickname),
             HttpStatus.OK
@@ -54,50 +65,69 @@ public class UserController implements UserControllerDocs {
     }
 
     @Override
-    public ResponseEntity<UpdateUserResponse> updateUserNickname(String id,
-        UpdateNicknameRequest request) {
-        log.info("Updating user nickname with id : {}", id);
+    public ResponseEntity<UpdateUserResponse> updateUserNickname(String requestUserId,
+        UpdateNicknameRequest updateNicknameRequest) {
+        log.info("Updating user nickname. User ID : {} Updating Nickname : {} ", requestUserId,
+            updateNicknameRequest.nickname());
+
         return new ResponseEntity<>(
-            userApplicationUseCase.updateUserNickname(id, request),
+            userApplicationUseCase.updateUserNickname(requestUserId, updateNicknameRequest),
             HttpStatus.OK
         );
     }
 
     @Override
-    public ResponseEntity<UpdateUserResponse> updateUserBirthday(String id,
-        UpdateBirthdayRequest request) {
-        log.info("Updating user birthday with id : {}", id);
+    public ResponseEntity<UpdateUserResponse> updateUserBirthday(String requestUserId,
+        UpdateBirthdayRequest updateBirthdayRequest) {
+        log.info("Updating user birthday. User ID : {} Updating Birthday {}", requestUserId,
+            updateBirthdayRequest.birthday());
+
         return new ResponseEntity<>(
-            userApplicationUseCase.updateUserBirthday(id, request),
+            userApplicationUseCase.updateUserBirthday(requestUserId, updateBirthdayRequest),
             HttpStatus.OK
         );
     }
 
     @Override
-    public ResponseEntity<UpdateUserResponse> updateUserMessage(String id,
-        UpdateMessageRequest request) {
-        log.info("Updating user message with id : {}", id);
+    public ResponseEntity<UpdateUserResponse> updateUserMessage(String requestUserId,
+        UpdateMessageRequest updateMessageRequest) {
+        log.info("Updating user message. User ID : {} Updating Message : {} ",
+            requestUserId, updateMessageRequest.message());
+
         return new ResponseEntity<>(
-            userApplicationUseCase.updateUserMessage(id, request),
+            userApplicationUseCase.updateUserMessage(requestUserId, updateMessageRequest),
             HttpStatus.OK
         );
     }
 
     @Override
-    public ResponseEntity<UpdateUserResponse> updateUserProfile(String id,
-        UpdateProfileImageUrlRequest request) {
-        log.info("Updating user profile with id : {}", id);
+    public ResponseEntity<UpdateUserResponse> updateUserProfileImage(String requestUserId,
+        UpdateProfileImageUrlRequest updateProfileImageUrlRequest) {
+        log.info("Updating user profile. User ID : {} Updating ImageUrl : {} ",
+            requestUserId, updateProfileImageUrlRequest.profileImageUrl());
+
         return new ResponseEntity<>(
-            userApplicationUseCase.updateUserProfileImage(id, request),
+            userApplicationUseCase.updateUserProfileImage(requestUserId,
+	updateProfileImageUrlRequest),
             HttpStatus.OK
         );
     }
 
     @Override
-    public ResponseEntity<DeleteUserResponse> deleteUser(String id) {
-        log.info("Deleting user with id : {}", id);
+    public ResponseEntity<DeleteUserResponse> deleteUser(String requestUserId) {
+        log.info("Deleting user with id : {}", requestUserId);
         return new ResponseEntity<>(
-            userApplicationUseCase.deleteUser(id),
+            userApplicationUseCase.deleteUser(requestUserId),
+            HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<RetrieveUserHomeResponse> retrieveUserHome(String requestUserId) {
+        log.info("Retrieving user home with id : {}", requestUserId);
+
+        return new ResponseEntity<>(
+            userApplicationUseCase.retrieveUserHome(requestUserId),
             HttpStatus.OK
         );
     }
--- user-service/src/main/java/kr/folio/user/presentation/rest/UserControllerDocs.java
+++ user-service/src/main/java/kr/folio/user/presentation/rest/UserControllerDocs.java
@@ -10,6 +10,7 @@ import kr.folio.user.presentation.dto.request.UpdateNicknameRequest;
 import kr.folio.user.presentation.dto.request.UpdateProfileImageUrlRequest;
 import kr.folio.user.presentation.dto.response.CreateUserResponse;
 import kr.folio.user.presentation.dto.response.DeleteUserResponse;
+import kr.folio.user.presentation.dto.response.RetrieveUserHomeResponse;
 import kr.folio.user.presentation.dto.response.UpdateUserResponse;
 import kr.folio.user.presentation.dto.response.ValidateUserResponse;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 
-@Tag(name = "유저 API", description = "유저 정보를 다루는 API")
+@Tag(name = "유저 API", description = "유저와 관련된 API")
 @Validated
 @RequestMapping("/users")
 public interface UserControllerDocs {
@@ -29,7 +30,7 @@ public interface UserControllerDocs {
     @Operation(summary = "유저 생성", description = "유저를 생성 합니다.")
     @PostMapping
     ResponseEntity<CreateUserResponse> createUser(
-        @RequestBody CreateUserRequest request);
+        @RequestBody CreateUserRequest createUserRequest);
 
     @Operation(summary = "아이디 중복 검증", description = "아이디 중복을 검증 합니다.")
     @GetMapping("/validation/id")
@@ -44,30 +45,35 @@ public interface UserControllerDocs {
     @Operation(summary = "유저 닉네임 변경", description = "유저 닉네임을 변경합니다.")
     @PostMapping("/nickname")
     ResponseEntity<UpdateUserResponse> updateUserNickname(
-        @CurrentUserId String id,
-        @RequestBody UpdateNicknameRequest request);
+        @CurrentUserId String requestUserId,
+        @RequestBody UpdateNicknameRequest updateNicknameRequest);
 
     @Operation(summary = "유저 생일 변경", description = "유저 생일을 변경합니다.")
     @PostMapping("/birthday")
     ResponseEntity<UpdateUserResponse> updateUserBirthday(
-        @CurrentUserId String id,
-        @RequestBody UpdateBirthdayRequest request);
+        @CurrentUserId String requestUserId,
+        @RequestBody UpdateBirthdayRequest updateBirthdayRequest);
 
     @Operation(summary = "유저 메시지 변경", description = "유저 상태 메시지를 변경합니다.")
     @PostMapping("/message")
     ResponseEntity<UpdateUserResponse> updateUserMessage(
-        @CurrentUserId String id,
-        @RequestBody UpdateMessageRequest request);
+        @CurrentUserId String requestUserId,
+        @RequestBody UpdateMessageRequest updateMessageRequest);
 
     @Operation(summary = "유저 프로필 사진 변경", description = "유저 프로필 사진을 변경합니다.")
     @PostMapping("/profile")
-    ResponseEntity<UpdateUserResponse> updateUserProfile(
-        @CurrentUserId String id,
-        @RequestPart UpdateProfileImageUrlRequest request);
+    ResponseEntity<UpdateUserResponse> updateUserProfileImage(
+        @CurrentUserId String requestUserId,
+        @RequestPart UpdateProfileImageUrlRequest updateProfileImageUrlRequest);
 
     @Operation(summary = "유저 삭제", description = "유저를 삭제합니다.")
     @DeleteMapping
     ResponseEntity<DeleteUserResponse> deleteUser(
-        @CurrentUserId String id);
+        @CurrentUserId String requestUserId);
+
+    @Operation(summary = "유저 홈 조회", description = "유저 홈을 조회합니다.")
+    @GetMapping("/home")
+    ResponseEntity<RetrieveUserHomeResponse> retrieveUserHome(
+        @CurrentUserId String requestUserId);
 
 }
