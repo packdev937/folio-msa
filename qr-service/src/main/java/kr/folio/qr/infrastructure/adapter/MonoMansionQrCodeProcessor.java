3376dc0b4a965821cb0848817a5430abebd66e75
Geonwoo Pack <packdev937@gmail.com>
2024-08-31 10:31:00 +0900

    feat (Qr-Service) : MonoMansion Strategy Pattern 추가한다

---
+++ qr-service/src/main/java/kr/folio/qr/infrastructure/adapter/MonoMansionQrCodeProcessor.java
@@ -0,0 +1,33 @@
+package kr.folio.qr.infrastructure.adapter;
+
+import kr.folio.qr.infrastructure.exception.PhotoQrUrlExpiredException;
+import kr.folio.qr.domain.core.strategy.QrCodeProcessor;
+import kr.folio.qr.infrastructure.util.WebClientUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class MonoMansionQrCodeProcessor implements QrCodeProcessor {
+    private final WebClient webClient;
+
+    @Override
+    public Mono<byte[]> extractImageFromQrUrl(String qrUrl) {
+        String qrCode = extractQrCodeFromUrl(qrUrl);
+        String imageUrl = String.format("https://monomansion.net/api/download.php?qrcode=%s&type=P", qrCode);
+        return WebClientUtil
+                .getBlob(webClient, imageUrl)
+                .onErrorMap(e -> new PhotoQrUrlExpiredException());
+    }
+
+    private String extractQrCodeFromUrl(String url) {
+        return UriComponentsBuilder
+                .fromUriString(url)
+                .build()
+                .getQueryParams()
+                .getFirst("qrcode");
+    }
+}
