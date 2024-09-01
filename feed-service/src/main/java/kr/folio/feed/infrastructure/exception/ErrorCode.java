0c1b1c02f1efbd92fda416d327fedeedbd2ead29
Geonwoo Pack <packdev937@gmail.com>
2024-09-01 11:31:09 +0900

    feat : Global Exception Handler를 추가한다

--- feed-service/src/main/java/kr/folio/feed/infrastructure/exception/ErrorCode.java
+++ feed-service/src/main/java/kr/folio/feed/infrastructure/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    FEED_NOT_FOUND("FD0001", "피드를 찾을 수 없습니다");
+    FEED_NOT_FOUND("FD0001", "피드를 찾을 수 없습니다"),
+    REQUEST_INPUT_NOT_VALID("EX0001", "입력 값이 올바르지 않습니다");
 
     private final String code;
     private final String message;
--- photo-service/src/main/java/kr/folio/photo/infrastructure/exception/ErrorCode.java
+++ photo-service/src/main/java/kr/folio/photo/infrastructure/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     PHOTO_NOT_FOUND("PT0001", "포토를 찾을 수 없습니다"),
     PHOTO_NOT_CREATED("PT0002", "포토를 생성할 수 없습니다"),
-    PHOTO_NOT_UPDATED("PT0003", "포토를 수정할 수 없습니다");
+    PHOTO_NOT_UPDATED("PT0003", "포토를 수정할 수 없습니다"),
+
+    REQUEST_INPUT_NOT_VALID("EX0001", "입력 값이 올바르지 않습니다");
 
     private final String code;
     private final String message;
