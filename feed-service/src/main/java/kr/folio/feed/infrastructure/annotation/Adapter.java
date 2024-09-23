51ba63a4017ab233cb64d9133a1ee01228de1835
Geonwoo Pack <packdev937@gmail.com>
2024-09-23 14:57:55 +0900

    refactor : @Adapter가 Runtime에 인식될 수 있도록 리팩토링한다

--- feed-service/src/main/java/kr/folio/feed/infrastructure/annotation/Adapter.java
+++ feed-service/src/main/java/kr/folio/feed/infrastructure/annotation/Adapter.java
@@ -1,7 +1,13 @@
 package kr.folio.feed.infrastructure.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.stereotype.Component;
 
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 @Component
 public @interface Adapter {
 
