798a2ddfd13ab031fa5898865e02e5bb42667137
Geonwoo Pack <packdev937@gmail.com>
2024-09-24 21:55:21 +0900

    refactor (Photo-Service) : Cloud Config Server를 사용하도록 리팩토링 한다

--- photo-service/src/test/java/kr/folio/photo/infrastructure/publisher/FakePhotoMessagePublisher.java
+++ photo-service/src/test/java/kr/folio/photo/infrastructure/publisher/FakePhotoMessagePublisher.java
@@ -1,11 +1,14 @@
 package kr.folio.photo.infrastructure.publisher;
 
+import java.util.function.BiConsumer;
 import kr.folio.infrastructure.kafka.producer.GenericEventPublisher;
+import kr.folio.infrastructure.outbox.OutboxStatus;
 
 public class FakePhotoMessagePublisher implements GenericEventPublisher<Object> {
 
+
     @Override
-    public void publish(Object event) {
-        // do nothing
+    public void publish(Object event, BiConsumer<Object, OutboxStatus> outboxCallback) {
+        return;
     }
 }
