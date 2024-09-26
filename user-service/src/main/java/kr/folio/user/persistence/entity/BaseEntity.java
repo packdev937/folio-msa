137f2fd8369a49593c360ee58948a6b464cf2a6e
Geonwoo Pack <packdev937@gmail.com>
2024-09-26 14:06:48 +0900

    refactor : BaseEntity를 리팩토링 한다

--- user-service/src/main/java/kr/folio/user/persistence/entity/BaseEntity.java
+++ user-service/src/main/java/kr/folio/user/persistence/entity/BaseEntity.java
@@ -1,19 +1,25 @@
 package kr.folio.user.persistence.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EntityListeners(AuditingEntityListener.class)
 @Getter
-@Setter
-public abstract class BaseEntity {
+@MappedSuperclass
+public class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false, name = "created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
 }
